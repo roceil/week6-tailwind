9b522fbd90fa3e70c086464f5d65e18757e5643b
roceil <a0978006326@gmail.com>
2022-08-16 22:16:05 +0800

    header&footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '0.75rem',
+        md:'4.75rem',
+      },
+    },
+    extend: {
+      colors:{
+        'primary':'#2D2D2D',
+        'secondary':'#62DB54',
+        'light-primary':'#909090',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
