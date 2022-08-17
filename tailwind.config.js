357b220bf4d7d7dfa8db7c0a6fdc6884022c6481
roceil <a0978006326@gmail.com>
2022-08-17 18:06:19 +0800

    學習程式RWD

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
         'primary':'#2D2D2D',
         'secondary':'#62DB54',
         'light-primary':'#909090',
-        'heavy-primary':'#202020'
+        'heavy-primary':'#202020',
+        'plus-heavy-primary':'#1C1C1C',
       }
     },
   },
