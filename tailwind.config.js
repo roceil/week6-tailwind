e97e02c24b94565e58ea3c0d9082d4b4deaf2678
roceil <a0978006326@gmail.com>
2022-08-19 01:15:16 +0800

    文章分享區塊完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       colors:{
         'primary':'#2D2D2D',
         'secondary':'#62DB54',
+        'third':'#09ACF5',
         'light-primary':'#909090',
         'heavy-primary':'#202020',
         'plus-heavy-primary':'#1C1C1C',
